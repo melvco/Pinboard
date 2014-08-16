41cdfb172529cc8cf521b3fc90c6dd5648275ed8
Cody Lee <melvco1@gmail.com>
2014-08-16 05:43:07 -0500

    fixed rake bug, implemented devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :views
   devise_for :users
+  devise_for :views
   root "pages#home"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
