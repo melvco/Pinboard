2833fd1994223950cf37d55595a02a8debf00b91
Cody Lee <melvco1@gmail.com>
2014-08-26 02:24:55 -0500

    added pagination

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,7 +2,7 @@ class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @pins = Pin.all.order("created_at DESC").limit(20)
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 20)
   end
 
   def show
